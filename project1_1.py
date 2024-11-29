fa3bbf3c41fb17941278d71fb79ce3f35892119d
soramametaro <soranohara416@gmail.com>
2024-11-29 16:18:35 +0900

    first commit

---
+++ project1_1.py
@@ -0,0 +1,6 @@
+def str2words(doc, stop):
+    words = []
+    for token in T.tokenize(doc):
+        if token.part_of_speech == '形容詞,自立' or token.part_of_speech == '動詞,自立' or token.part_of_speech == '名詞':
+            words.append(token.base_form)
+    important_words = [word for word in words if word != stop]
